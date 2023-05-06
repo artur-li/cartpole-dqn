89200db4e5011bd3c62798074e6d5edaf8eeb010
Artur <arturli.official@gmail.com>
2023-05-06 00:05:15 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,94 @@
+import gym
+from keras import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+from collections import deque
+import random
+import numpy as np
+
+env = gym.make('CartPole-v1')
+
+# paramaters
+batch_size = 32
+n_episodes = 1000
+
+# agent
+class DQNAgent:
+    def __init__(self):
+        self.model = self.build_model()
+        self.memory = deque(maxlen=2000)
+        self.epsilon = 1
+        self.epsilon_decay = 0.995
+        self.epsilon_min = 0.001
+        self.discount_factor = 0.99
+        self.learning_rate = 0.001
+
+    def build_model(self):
+        model = Sequential()
+        model.add(Dense(24, input_dim=4, activation='relu'))  # Input layer
+        model.add(Dense(24, activation='relu'))               # Hidden layer
+        model.add(Dense(2, activation='linear'))              # Output layer
+        model.compile(loss='mse', optimizer=Adam(learning_rate=0.1))
+        return model
+
+    def act(self, state):
+        if self.epsilon > random.random():
+            action = env.action_space.sample()
+        else: 
+            state = state.reshape(1, -1)  # reshape the state input
+            action = np.argmax(self.model.predict(state, verbose=0))
+        return action
+    
+    def remember(self, state, action, reward, done, new_state):
+        self.memory.append((state, action, reward, done, new_state))
+
+    def replay(self, batch_size):
+        if len(self.memory) < batch_size:
+            return
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, done, new_state in minibatch:
+            target = reward
+            if not done:
+                new_state = new_state.reshape(1, -1)  # reshape the new_state input
+                target = (reward + self.discount_factor * np.amax(self.model.predict(new_state, verbose=0)[0])) 
+            state = state.reshape(1, -1)  # reshape the state input
+            target_f = self.model.predict(state, verbose=0)
+            target_f[0][action] = target
+            self.model.fit(state, target_f, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+agent = DQNAgent()  # initialize agent
+
+# TRAINING
+for i in range(n_episodes):
+
+    state, info = env.reset()  
+    score = 0
+    done = False  
+    
+    while not done:
+
+        # pick action
+        action = agent.act(state)  
+
+        # execute action
+        new_state, reward, done, _, _ = env.step(action)
+
+        # store in replay memory
+        agent.remember(state, action, reward, done, new_state)
+
+        # update state
+        state = new_state
+
+        # add 1 to score
+        score += 1
+    
+    # learn from replay memory
+    agent.replay(batch_size)
+
+    # print info about episode
+    print(f"episode: {i+1}, score: {score}, epsilon: {agent.epsilon}")
+
+    # reset score
+    score = 0
